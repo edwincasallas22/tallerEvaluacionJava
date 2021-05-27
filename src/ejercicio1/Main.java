946303b78e12d94dae818381167305d5e40a87e3
Edwin Casallas <casallas22@hotmail.com>
2021-05-27 02:04:10 -0500

    Se realizo el primer ejercicio en el cual se comparaba cual de los dos numeros era mayor o si estos eran iguales en valor

--- src/ejercicio1/Main.java
+++ src/ejercicio1/Main.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        int num1 = 7;
+        int num2 = 20;
+        if(num1 > num2){
+            System.out.println("El numero "+ num1+ " es mayor que el numero " + num2);
+        }
+        else if (num2 > num1){
+            System.out.println("El numero "+ num2+ " es mayor que el numero " + num1);
+        }
+        else{
+            System.out.println("El numero "+ num1+ " es igual al numero " + num2);
+        }
     }
 }
